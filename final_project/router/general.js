a4d88ba73a6e59e088fdd32e085630cc47b56061
Julien de Tourris <liberal.it.nz@gmail.com>
2025-05-01 13:09:18 +0200

    Update general.js task 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -79,39 +79,44 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
 
 public_users.get('/author/:author', function (req, res) {
-  const authorName = req.params.author;
-  const matchingBooks = [];
-
-  // Get all ISBN keys from the books object
-  const isbns = Object.keys(books);
-
-  // Iterate through all books
-  for (const isbn of isbns) {
-    const book = books[isbn];
-    // Check if author matches (case insensitive)
-    if (book.author.toLowerCase().includes(authorName.toLowerCase())) {
-      matchingBooks.push({
-        isbn: isbn,
-        title: book.title,
-        author: book.author, // Include author in response for clarity
-        reviews: book.reviews
-      });
-    }
-  }
-
-  if (matchingBooks.length > 0) {
-    // Return matching books with success status
-    return res.status(200).json({
+  const authorName = req.params.author.toLowerCase();
+  
+  new Promise((resolve, reject) => {
+    process.nextTick(() => {
+      const matchingBooks = [];
+      const isbns = Object.keys(books);
+      
+      for (const isbn of isbns) {
+        const book = books[isbn];
+        if (book.author.toLowerCase().includes(authorName)) {
+          matchingBooks.push({
+            isbn: isbn,
+            title: book.title,
+            author: book.author,
+            reviews: book.reviews
+          });
+        }
+      }
+      
+      if (matchingBooks.length > 0) {
+        resolve(matchingBooks);
+      } else {
+        reject(new Error(`No books found by author containing: "${req.params.author}"`));
+      }
+    });
+  })
+  .then(matchingBooks => {
+    res.status(200).json({
       message: `${matchingBooks.length} book(s) found`,
       books: matchingBooks
     });
-  } else {
-    // Return error if no books found
-    return res.status(404).json({ 
-      message: `No books found by author containing: "${authorName}"`,
+  })
+  .catch(err => {
+    res.status(404).json({ 
+      message: err.message,
       suggestion: "Please check the spelling or try a partial author name"
     });
-  }
+  });
 });
 
 public_users.get('/title/:title', function (req, res) {
